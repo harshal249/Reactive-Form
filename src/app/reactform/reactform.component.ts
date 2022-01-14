f92ec7d79adb281f5018b586892fb392b8081006
HarshalP <harshalpote08@gmail.com>
2022-01-14 14:22:03 +0530

    final commit

--- src/app/reactform/reactform.component.ts
+++ src/app/reactform/reactform.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormControl } from '@angular/forms';
+import { FormGroup,FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'reactform',
@@ -9,11 +10,22 @@ import { FormGroup,FormControl } from '@angular/forms';
 export class ReactformComponent  {
 
   form= new FormGroup({
-    projectname : new FormControl(),
-    projectcode : new FormControl(),
-    projectstartdate : new FormControl(),
-    projectenddate : new FormControl()
-
+    projectname : new FormControl('',Validators.required),
+    projectcode : new FormControl('',Validators.required),
+    projectstartdate : new FormControl('',Validators.required),
+    projectenddate : new FormControl('',Validators.required)
   });
 
+  get projectname() { return this.form.get('projectname'); }
+  get projectcode() { return this.form.get('projectcode'); }
+  get projectstartdate() {return this.form.get('projectstartdate');}
+  get projectenddate() {return this.form.get('projectenddate');}
+
+  onSubmit(){
+    if(this.form.valid){
+      console.log("Form Succesfully Submitted");
+      this.form.reset()
+    }
+    console.log("Please Fill the details")
+  }
 }
